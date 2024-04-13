1c21e43be539409a5da2ca8f8b03f931a1736feb
tohkarle <tohkarle@hotmail.com>
2024-04-14 05:15:39 +0800

    feat: Change icon in settings to fix build error

--- src/pages/SettingsPage/Settings.jsx
+++ src/pages/SettingsPage/Settings.jsx
@@ -1,11 +1,10 @@
-import { Menu, Transition } from '@headlessui/react';
-import React, { Fragment, useState, useEffect } from 'react';
-import { IoChevronDownOutline, IoInvertMode } from "react-icons/io5";
+import React from 'react';
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
 import ToolBarNew from '../../components/ToolBarNew';
 import { useThemeContext } from '../../context/ColorModeContext';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
+
+
 
 const Settings = () => {
 
@@ -24,15 +23,17 @@ const Settings = () => {
                     Appearance
                 </div>
 
-                <div className="flex items-center">
-                    {theme.palette.mode}
-                    <button
-                        onClick={toggleTheme}
-                        className="bg-transparent py-0 pl-2 pr-0 border-none"
-                    >
-                        {theme.palette.mode === 'dark' ? <Brightness4Icon /> : <Brightness7Icon />}
-                    </button>
-                </div>
+                <button
+                    onClick={toggleTheme}
+                    className="bg-transparent py-0 pl-2 pr-0 border-none"
+                >
+                    <div className="flex items-center">
+                        {theme.palette.mode}
+
+                        {theme.palette.mode === 'dark' ? <MdDarkMode className="w-6 h-6 ml-1" /> : <MdLightMode className="w-6 h-6 ml-1" />}
+
+                    </div>
+                </button>
 
             </div>
             <ToolBarNew />
